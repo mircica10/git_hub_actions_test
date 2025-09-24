b1bc05e9e5cbac964379907bb03fc0530a04bfa4
mircica10 <mircica10@yahoo.com>
2025-09-24 14:00:09 +0200

    Update TestBasic.py

--- tests/TestBasic.py
+++ tests/TestBasic.py
@@ -2,21 +2,21 @@ import unittest
 
 class TestBasic(unittest.TestCase):
   
-  def setUp(self):
-    pass
+    def setUp(self):
+        pass
 
-  def test_sum(self):
-    v1, v2 = 4, 5    
-    v3 = v1 + v2
-    assert v3 == 9, 'sum should be 9'
+    def test_sum(self):
+        v1, v2 = 4, 5    
+        v3 = v1 + v2
+        assert v3 == 9, 'sum should be 9'
 
-  def test_mul(self):
-    v1, v2 = 3, 4
-    v3 = v1 * v2
-    assert v3 == 12, 'mul should be 12'
+    def test_mul(self):
+        v1, v2 = 3, 4
+        v3 = v1 * v2
+        assert v3 == 12, 'mul should be 12'
   
-  def tearDown(self):
-    pass
+    def tearDown(self):
+        pass
     
 
 if __name__ == '__main__':
