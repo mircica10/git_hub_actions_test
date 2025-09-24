b5dfc6ca583e2603727399331609157e0359d0b1
mircica10 <mircica10@yahoo.com>
2025-09-24 14:10:10 +0200

    Update TestBasic.py

--- tests/TestBasic.py
+++ tests/TestBasic.py
@@ -2,17 +2,17 @@ import unittest
 
 class TestBasic(unittest.TestCase):
 
-  
+
     def setUp(self):
         pass
 
-  
+
     def test_sum(self):
         v1, v2 = 4, 5    
         v3 = v1 + v2
         assert v3 == 9, 'sum should be 9'
 
-  
+
     def test_mul(self):
         v1, v2 = 3, 4
         v3 = v1 * v2
