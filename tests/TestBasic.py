fcd59c253c4b403f5dbd81eeed83ad4d6e849ca0
mircica10 <mircica10@yahoo.com>
2025-09-24 14:03:54 +0200

    Update TestBasic.py

--- tests/TestBasic.py
+++ tests/TestBasic.py
@@ -11,10 +11,12 @@ class TestBasic(unittest.TestCase):
         v3 = v1 + v2
         assert v3 == 9, 'sum should be 9'
 
+  
     def test_mul(self):
         v1, v2 = 3, 4
         v3 = v1 * v2
         assert v3 == 12, 'mul should be 12'
+
   
     def tearDown(self):
         pass
